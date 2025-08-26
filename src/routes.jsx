131cd61021692b6ed694b2203b600b52cb8b133e
samuel <samawyck@gmail.com>
2025-08-26 17:41:21 -0500

    Add mainPage as a child route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,20 @@
 import App from "./App.jsx";
 import AuthPage from "./components/authPage.jsx";
+import MainPage from "./components/mainPage.jsx";
 
 
 
 const routes = [
     {
         path: "/",
-        element: <App />
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                index: true,
+                element: <MainPage />
+            }
+        ]
     },
     {
         path: "/signup",
