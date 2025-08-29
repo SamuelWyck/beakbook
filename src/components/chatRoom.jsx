466a8af363df18aef5b098ceed3b46eb80817f44
samuel <samawyck@gmail.com>
2025-08-29 17:01:20 -0500

    Fix socket null problem and add new messages to the chat

--- src/components/chatRoom.jsx
+++ src/components/chatRoom.jsx
@@ -2,40 +2,100 @@ import "../styles/chatRoom.css";
 import closeImg from "../assets/close.svg";
 import { useEffect, useState } from "react";
 import apiManager from "../utils/apiManager.js";
+import MessageCard from "./messageCard.jsx";
+import LoadingPage from "./loadingPage.jsx";
 
 
 
-function ChatRoom({roomId, handleClose}) {
+function ChatRoom({roomId, handleClose, userId}) {
+    const [messages, setMessages] = useState(null);
     const [webSocket, setWebSocket] = useState(null);
 
+
+    useEffect(function() {
+        if (!messages) {
+            return;
+        }
+
+        const messageEle = document.querySelector(
+            ".messages"
+        );
+        messageEle.scrollBy({
+            top: messageEle.scrollHeight,
+            behavior: "instant"
+        });
+    }, [messages])
+    
+    
     useEffect(function() {
         if (!roomId) {
             return;
         }
+        
+        apiManager.getChatMessages(roomId).then(function(res) {
+            if (res.errors) {
+                //error popup
+            }
+            setMessages(getMessageCards(res.messages));
 
-        const url = apiManager.getSocketUrl(roomId);
-        const socket = new WebSocket(url);
-        console.log(socket)
-        socket.onmessage = function(event) {
-            const data = JSON.parse(event.data);
-            console.log(data);
-        };
-        setWebSocket(socket);
+            const url = apiManager.getSocketUrl(roomId);
+            const socket = new WebSocket(url);
+            socket.onmessage = onMessage;
+            setWebSocket(socket);
+        });
 
 
         document.addEventListener("keydown", triggerSubmit);
     }, [roomId]);
 
     
+    function onMessage(event) {
+        const message = JSON.parse(event.data);
+        if (message.authorId === userId) {
+            cleanForm();
+        }
+
+        const msgCard = <MessageCard
+            key={message.id}
+            msg={message}
+            userId={userId}
+        />;
+        setMessages(messages => [...messages, msgCard]);
+    };
+
+
+    function cleanForm() {
+        const msgForm = document.querySelector(
+            ".chat-messages form"
+        );
+        msgForm.reset();
+    };
+
+
+    function getMessageCards(messages) {
+        const msgCards = [];
+        for (let message of messages) {
+            msgCards.push(
+                <MessageCard
+                    key={message.id}
+                    msg={message}
+                    userId={userId}
+                />
+            );
+        }
+        return msgCards;
+    };
+
+    
     function triggerSubmit(event) {
         if (event.key !== "Enter" || event.shiftKey) {
             return;
         }
-        event.preventDefault();
         const textArea = document.querySelector("#message");
         if (document.activeElement !== textArea) {
             return;
         }
+        event.preventDefault();
         if (textArea.value.trim() === "") {
             return;
         }
@@ -58,8 +118,6 @@ function ChatRoom({roomId, handleClose}) {
         }
         reqBody = JSON.stringify(reqBody);
         webSocket.send(reqBody);
-
-        event.target.reset();
     };
 
 
@@ -72,7 +130,11 @@ function ChatRoom({roomId, handleClose}) {
 
 
     if (!roomId || !webSocket) {
-        return null;
+        return (
+            <div className="chat-messages">
+                <LoadingPage />
+            </div>
+        );
     }
 
 
@@ -84,7 +146,7 @@ function ChatRoom({roomId, handleClose}) {
                 </button>
             </div>
             <div className="messages">
-
+                {messages}
             </div>
             <form onSubmit={submitMsg}>
                 <textarea 
