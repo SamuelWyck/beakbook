7eb73cc5cddef4afd3a14bf4507652d8fb873316
samuel <samawyck@gmail.com>
2025-09-10 11:04:51 -0500

    Add logic to open chats in options menu

--- src/components/chatUserCard.jsx
+++ src/components/chatUserCard.jsx
@@ -7,7 +7,16 @@ import apiManager from "../utils/apiManager.js";
 
 
 
-function ChatUserCard({user, userId, closeCb, requestCb}) {
+function ChatUserCard(
+    {
+        user, 
+        userId, 
+        chatCloseCb, 
+        requestCb, 
+        socket, 
+        modalCloseCb,
+        statusCb
+    }) {
     const friendsRef = useContext(FriendsContext);
     const [render, setRender] = useState(false);
 
@@ -21,6 +30,10 @@ function ChatUserCard({user, userId, closeCb, requestCb}) {
         if (userId === user.id) {
             return;
         }
+        if (event.target.matches(".options-modal")) {
+            return;
+        }
+
         event.stopPropagation();
         const target = eleFromPoint(
             event.clientX, 
@@ -49,7 +62,7 @@ function ChatUserCard({user, userId, closeCb, requestCb}) {
             );
             friendBtn.click();
         }
-        closeCb();
+        chatCloseCb();
     };
 
 
@@ -63,6 +76,7 @@ function ChatUserCard({user, userId, closeCb, requestCb}) {
             reqBody
         );
         if (res.errors) {
+            statusCb("Unable to add friend");
             return;
         }
         friendsRef.current.add(user.id);
@@ -71,6 +85,44 @@ function ChatUserCard({user, userId, closeCb, requestCb}) {
             res.friendRequest, 
             res.friendRequest.receivingUserId
         );
+    };
+
+
+    async function findUserDM(event) {
+        event.stopPropagation();
+        let reqBody = {
+            ids: [user.id]
+        };
+        reqBody = JSON.stringify(reqBody);
+
+        const res = await apiManager.findUserDM(reqBody);
+        if (res.errors) {
+            statusCb("Unable to open chat");
+            return;
+        }
+
+        if (!res.found) {
+            const roomIds = [];
+            for (let user of res.chat.users) {
+                roomIds.push(user.id);
+            }
+            socket.emit("add-chat", res.chat, roomIds);
+            socket.once("add-chat", function() {
+                setTimeout(function() {
+                    chatCloseCb();
+                    const roomBtn = document.querySelector(
+                        `.chat-btn[data-chatid="${res.chat.id}"]`
+                    );
+                    roomBtn.click();
+                }, 1);
+            });
+        } else {
+            modalCloseCb();
+            const roomBtn = document.querySelector(
+                `.chat-btn[data-chatid="${res.chat.id}"]`
+            );
+            roomBtn.click();
+        }
     };
 
 
@@ -93,6 +145,7 @@ function ChatUserCard({user, userId, closeCb, requestCb}) {
                     onClick={goToFriendList}
                 >Remove friend</button>
                 }
+                <button onClick={findUserDM}>Message</button>
             </span>
             <div className="chat-user-profile-wrapper">
                 {(user.profileImg) ?
